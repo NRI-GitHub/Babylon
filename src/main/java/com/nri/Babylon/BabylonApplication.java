468cfea19e4fda3ed9f89af47c6b328fa8867c2b
Jeff Jones <jeffthejava@gmail.com>
2023-06-11 16:17:36 -0400

    Frist init commit (fyi have not run)

---
+++ src/main/java/com/nri/Babylon/BabylonApplication.java
@@ -0,0 +1,13 @@
+package com.nri.Babylon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabylonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabylonApplication.class, args);
+	}
+
+}
