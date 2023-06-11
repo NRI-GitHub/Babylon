d6b707213f5737ed7dce0813cbb926ea2ecfefdf
Jeff Jones <jeffthejava@gmail.com>
2023-06-11 16:24:43 -0400

    Test full run on the web application. Running on port 8080 working

--- src/main/java/com/nri/babylon/BabylonApplication.java
+++ src/main/java/com/nri/babylon/BabylonApplication.java
@@ -1,4 +1,4 @@
-package com.nri.Babylon;
+package com.nri.babylon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nri/babylon/BabylonApplicationTests.java
+++ src/test/java/com/nri/babylon/BabylonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nri.Babylon;
+package com.nri.babylon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
