7d2a4cbc61e0db30e10b4b1f9f224acc2934c592
Jeff Jones <jeffthejava@gmail.com>
2023-06-14 22:13:12 -0400

    WIP: working on new register page

--- src/main/java/com/nri/babylon/view/HttpController.java
+++ src/main/java/com/nri/babylon/view/HttpController.java
@@ -3,13 +3,20 @@ package com.nri.babylon.view;
 import com.nri.babylon.testing.TestLibs;
 import com.nri.library.text_translation.NRITextTranslation;
 import com.nri.library.tts.NRITextToSpeech;
+import org.kurento.tutorial.groupcall.UserSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HttpController {
+    private static final Logger log = LoggerFactory.getLogger(HttpController.class);
 
     @Autowired
     private NRITextToSpeech nriTextToSpeech;
@@ -21,12 +28,12 @@ public class HttpController {
     private TestLibs testLibs;
 
     @GetMapping("/")
-    public String getIndex(){
+    public String getIndex() {
         return "index";
     }
 
     @GetMapping("/test-libs")
-    public String getTestLibs(){
+    public String getTestLibs() {
         testLibs.printTest();
         return "test-lib";
     }
@@ -36,11 +43,46 @@ public class HttpController {
         return "audioTest";
     }
 
+    @GetMapping("/register")
+    public String register() {
+        return "register-page";
+    }
+
 
     @GetMapping("/call")
     public String getIndex2(Model model) {
         model.addAttribute("voices", nriTextToSpeech.getVoices());
         model.addAttribute("languages", nriTextTranslation.getSupportedLanguages());
         return "index2";
+    }
+
+
+    @GetMapping("/call2")
+    public String getCall2(Model model,
+                           @RequestParam(name = "name") String name, @RequestParam(name = "room") String room,
+                           @RequestParam(name = "voiceId") String voiceId, @RequestParam(name = "languageId") String languageId) {
+
+        log.info("request param: name : " + name);
+        log.info("request param: room : " + room);
+        log.info("request param: voiceId : " + voiceId);
+        log.info("request param: languageId : " + languageId);
+        model.addAttribute("voices", nriTextToSpeech.getVoices());
+        model.addAttribute("languages", nriTextTranslation.getSupportedLanguages());
+        return "index2";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(Model model) {
+        String name = "testName";
+        String room = "testRoom";
+        String voiceId = "testVoiceId";
+        String languageId = "testLangId";
+
+
+        String requestParam =
+                "?name=" + name + "&" + "room="
+                        + room + "&" + "voiceId="
+                        + voiceId + "&" + "languageId=" + languageId;
+        return "redirect:/call2" +requestParam;
     }
 }
